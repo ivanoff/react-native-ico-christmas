456008a6f4adcbeb3c2513a9c1ef0f0e25edf711
Dimitry Ivanov <2@ivanoff.org.ua>
2023-09-09 22:45:59 +0300

    add types

--- typings.d.ts
+++ typings.d.ts
@@ -102,10 +102,35 @@ declare module 'react-native-ico-christmas' {
       'mrs-claus' |
       'santa-claus';
 
+    type backgroundType = 'circle' | 'rect' | 'button';
+
+    type positionType = 'top' | 'bottom' | 'right' | 'left' |
+      'top_right' | 'top_left' | 'bottom_right' | 'bottom_left';
+
+    type backgroundTypeObj = {
+      type?: backgroundType;
+      color?: string;
+      radius?: number;
+      borderRadius?: number;
+    };
+
+    type badgeTypeObj = {
+      value?: number | string;
+      fontSize?: string;
+      position?: positionType;
+      color?: string;
+      backgroundColor?: string;
+      radius?: number;
+    };
+
     interface IconProps {
       name: iconNames;
-      color: string;
-      size: number;
+      width?: number;
+      height?: number;
+      color?: string;
+      stroke?: number;
+      background?: backgroundType | backgroundTypeObj;
+      badge?: number | string | badgeTypeObj;
     }
 
     const Icon: (props: IconProps) => ReactNode;
@@ -113,4 +138,3 @@ declare module 'react-native-ico-christmas' {
     export { iconNames };
     export default Icon;
 }
-
